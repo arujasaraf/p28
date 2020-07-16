1b5251b4b6b7d2c45727aafaeb0b14228d35a817
arujasaraf <65903153+arujasaraf@users.noreply.github.com>
2020-07-16 14:04:28 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+    class Paper{
+    constructor(x, y) {
+        var options = {
+            'isStatic':false,
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2
+        }
+        this.image= loadImage("sprites/paper.png");
+
+        this.body =Bodies.circle(200 , 200 , 20,options);
+        this.radius=20;
+        
+        World.add(world, this.body);
+        }
+        display(){
+            var pos =this.body.position;
+            var angle=this.body.angle
+            push();
+            translate(pos.x,pos.y);
+            rotate(angle)
+           // ellipseMode(RADIUS);
+            //ellipse(0,0, this.radius, this.radius);
+            imageMode(CENTER);
+            image(this.image, 0, 0, this.radius ,this.radius)
+            pop();
+        }1
+}
